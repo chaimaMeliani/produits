55bd2d0ec384f384c6a292847544b0909b4318b7
chaima meliani <chaima.meliani@sesame.com.tn>
2018-10-25 18:06:29 +0200

    init

---
+++ microServiceProduit/src/main/java/com/example/demo/controller/ImplementationService.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Dao.ProduitRepository;
+import com.example.demo.entities.Produit;
+
+@RestController
+public class ImplementationService {
+	@Autowired
+	private ProduitRepository produitRepository;
+	
+	@RequestMapping("/save")
+	public Produit saveProduit(Produit p) {
+		
+		produitRepository.save(p);
+		return p ;
+	}
+	
+	
+	@RequestMapping("/all")
+	public List <Produit> getProduits(){
+		
+		
+		return produitRepository.findAll();
+		
+	}
+
+
+	@RequestMapping("/produits")
+
+	public Page <Produit> getProduits(int page){
+
+		
+	return produitRepository.findAll( PageRequest.of(0, 3));	
+	}
+}
